c37f698c1998c5a968fe2c6541996ea00fc94f5a
smendez92 <samuelremndez14@gmail.com>
2017-08-14 22:02:23 -0500

    env file setup

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+//access environmental variables for username, password, host
+require('dotenv').config({path: '../dotenv.env'});
+
+var connection = mysql.createConnection({
+  port: process.env.DB_port || 3306,
+  host:  process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "cat_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
