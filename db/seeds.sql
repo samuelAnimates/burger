b5b562171fd89deaad3b082def98bf1da3396c9f
smendez92 <samuelremndez14@gmail.com>
2017-08-14 23:25:20 -0500

    schema and seeds files created

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name) VALUES('Cauliflower is Cumin From Inside the House Burger');
+INSERT INTO burgers (burger_name) VALUES('The Silentil Night Burger');
+INSERT INTO burgers (burger_name) VALUES('Poutine on the Ritz Burger');
+INSERT INTO burgers (burger_name) VALUES('Poblano Picasso Burger');
+INSERT INTO burgers (burger_name) VALUES('Fifth Day of Christmas Burger');
